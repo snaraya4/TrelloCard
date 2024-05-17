90e7df7633177b608e2e955f698d4cc5c5883041
Sravya <snaraya4@binghamton.edu>
2024-05-17 12:26:19 -0400

    Added function to create a card

--- add_trello_card.py
+++ add_trello_card.py
@@ -20,13 +20,29 @@ def get_lists_from_board(board_id):
         print("Error fetching lists:", response.text)
         return None
     
+def create_card(list_id, card_name, card_desc):
+    url = "https://api.trello.com/1/cards"
+    query = {
+        'key': API_KEY,
+        'token': TOKEN, 
+        'idList': list_id,
+        'name': card_name,
+        'desc': card_desc
+    }
+
+    response = requests.post(url, data=query)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error creating card:", response.text)
+        return None
+    
 def main():
     parser = argparse.ArgumentParser(description='Create a Trello card with labels and a comment.')
     parser.add_argument('board_id', type=str, help='The ID of the Trello Board.')
     parser.add_argument('list_name', type=str, help='The name of the list to add the card to.')
     parser.add_argument('card_name', type=str, help='The name of the new card.')
-    parser.add_argument('card_desc', type=str, help='The description of the new card.')
-    parser.add_argument('comment_text', type=str, help='The comment to add to the new card.')
+    
 
 if __name__ == "__main__":
     main()
