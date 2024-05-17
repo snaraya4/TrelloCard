3eeb338ce986ae9d032fa999e5067705676ae242
Sravya <snaraya4@binghamton.edu>
2024-05-17 13:03:04 -0400

    Added function to create a label for the board

--- add_trello_card.py
+++ add_trello_card.py
@@ -51,11 +51,29 @@ def add_comment_to_card(card_id, comment_text):
     else:
         print("Error adding comment:", response.text)
 
+def create_label(board_id, label_name, label_color):
+    url = f"https://api.trello.com/1/boards/{board_id}/labels"
+    query = {
+        'key': API_KEY,
+        'token': TOKEN,
+        'name': label_name,
+        'color': label_color
+    }
+
+    response = requests.post(url, data=query)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error creating label:", response.text)
+        return None
+
+
 def main():
     parser = argparse.ArgumentParser(description='Create a Trello card with labels and a comment.')
     parser.add_argument('board_id', type=str, help='The ID of the Trello Board.')
     parser.add_argument('list_name', type=str, help='The name of the list to add the card to.')
     parser.add_argument('card_name', type=str, help='The name of the new card.')
+    parser.add_argument('card_desc', type=str, help='The description of the new card.')
     parser.add_argument('comment_text', type=str, help='The comment to add to the new card.')
     
 
